1ae1ecbedf91547e8eccfc8461904bcdcbc2607e
phucdeptrai <tranhoangphuchatien07@gmail.com>
2024-10-17 23:55:06 +0700

    Ql mã giảm giá

--- src/models/OrderProduct.js
+++ src/models/OrderProduct.js
@@ -23,6 +23,8 @@ const orderSchema = new mongoose.Schema({
     paymentMethod: {type: String, required: true},
     itemsPrice: {type: Number, required: true},
     shippingPrice: {type: Number, required: true},
+    discountCode: {type: String},
+    discountPercentage: {type: Number},
     totalPrice: {type: Number, required: true},
     user : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     isPaid: {type: Boolean, default: false},
