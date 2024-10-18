cded001d1b1e38e53aa640c07741964918af9549
Khoi <khoikhoi0911@gmail.com>
2024-10-18 10:43:25 +0700

    MOMO

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const routes = require('./routes');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+const crypto = require('crypto');
+
 const { default: axios } = require("axios");
 
 
@@ -44,15 +46,17 @@ app.listen(port, () => {
     console.log('Server running on port:', + port)
 })
 
+var accessKey = 'F8BBA842ECF85';
+var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
+//Tạo link thanh toán
 app.post("/payment", async (req, res) => {
     //https://developers.momo.vn/#/docs/en/aiov2/?id=payment-method
     //parameters
-    var accessKey = 'F8BBA842ECF85';
-    var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
+
     var orderInfo = 'pay with MoMo';
     var partnerCode = 'MOMO';
     var redirectUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
-    var ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+    var ipnUrl = 'https://1b9e-113-23-35-209.ngrok-free.app/callback';
     var requestType = "payWithMethod";
     var amount = '50000';
     var orderId = partnerCode + new Date().getTime();
@@ -95,16 +99,17 @@ app.post("/payment", async (req, res) => {
         signature : signature
     });
     
-    //option for axios
-    const options = {
-        method: "POST",
-        url:"https://test-payment.momo.vn/v2/gateway/api/create",
-        headers:{
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(requestBody)
-        },
-        data: requestBody 
-    }
+    //option for axios 
+    //sử dụng để cấu hình một yêu cầu POST với thư viện Axios nhằm gọi API của MoMo
+        const options = {
+            method: "POST",
+            url:"https://test-payment.momo.vn/v2/gateway/api/create",
+            headers:{
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(requestBody)
+            },
+            data: requestBody 
+        }
     
     let result;
         try
@@ -118,6 +123,48 @@ app.post("/payment", async (req, res) => {
             statusCode: 500,
             message: "server error"
         })
-    }
-    
+    } 
     })
+
+app.post("/callback", async(req, res) => {
+
+    console.log("callback:: ");
+    console.log(req.body);
+    //update order 
+    return res.status(200).json(req.body);
+    
+})
+
+// api check trang thai giao dich
+app.post("/transactionstatus", async(req,res)=>{
+const{orderId} = req.body;
+const rawSignature = `accessKey=${accessKey}&orderId=${orderId}&partnerCode=MOMO&requestId=${orderId}`;
+const signature = crypto
+    .createHmac("sha256",secretKey)
+    .update(rawSignature)
+    .digest('hex');
+    
+    const requestBody = JSON.stringify({
+        partnerCode:"MOMO",
+        requestId: orderId,
+        orderId: orderId,
+        signature: signature,
+        lang:'vi'
+    })
+
+    //option for axious
+    const options = {
+        method:"POST",
+        url:'https://test-payment.momo.vn/v2/gateway/api/query',
+        headers:{
+            'Content-Type': 'application/json'
+
+        },
+        data: requestBody
+    }
+    //call api 
+    let result = await axios(options);
+
+
+    return res.status(200).json(result.data);
+})
