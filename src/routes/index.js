b3c33f5d4638b554b93803992dea9db883a2b431
Khoi <khoikhoi0911@gmail.com>
2024-10-20 08:17:27 +0700

    Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,15 @@ const ProductRouter = require('./ProductRouter')
 const OrderRouter = require('./OrderRouter')
 const DiscountRouter = require('./DiscountRouter')
 const PaymentRouter = require('./PaymentRouter')
-const orderVNPAY = require('./orderVNPAY')
+
+
 const routes = (app) => {
     app.use('/api/user', UserRouter)
     app.use('/api/product', ProductRouter)
     app.use('/api/order', OrderRouter)
     app.use('/api/discount', DiscountRouter)
     app.use('/api/payment',PaymentRouter)
-    app.use('/api/vnpay',orderVNPAY)
+  
 
 }
 
