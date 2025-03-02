d235364b9a02d787d73da7cdd835b6f29c31787f
Sela <tranhoangphuchatien07@gmail.com>
2025-03-02 20:06:49 +0700

    update corsOptions

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5082;
 
 const corsOptions = {
-    origin: 'http://localhost:5085', // địa chỉ frontEnd
+    origin: process.env.FRONTEND_URL || 'http://localhost:5085', // địa chỉ frontEnd
     credentials: true, // Cho phép gửi cookie
 };
 
