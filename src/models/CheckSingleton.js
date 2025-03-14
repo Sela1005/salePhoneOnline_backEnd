072c3bd65173b51d90b5905b8790c9a14dee0caa
Sela <tranhoangphuchatien07@gmail.com>
2025-03-14 23:18:45 +0700

    checkSingleton

--- src/models/CheckSingleton.js
+++ src/models/CheckSingleton.js
@@ -2,4 +2,4 @@ const db1 = require('./Database');
 const db2 = require('./Database');
 
 // Nếu đúng Singleton, kết quả sẽ là true
-console.log("db1 === db2:", db1._connect === db2._connect); 
+console.log("db1 === db2:", db1.getInstance() === db2.getInstance()); 
